de2959f242b0b264fe260fdc9d14cd9a1f30bef9
Caver1K <168340347+Caver1K@users.noreply.github.com>
2025-04-06 08:30:55 +0200

    Encrypted + renamed the file

---
+++ Simple SSH Connection.cmd
@@ -0,0 +1,66 @@
+��
+cls
+@echo off
+chcp 65001 >nul
+title Simple SSH Connection made by Caver1K
+:Check
+set "filename=%TEMP%\plink.exe"
+
+IF EXIST "%filename%" (
+    goto Main
+) ELSE (
+    cd %TEMP%
+    curl -O https://the.earth.li/~sgtatham/putty/0.83/w64/plink.exe
+	 cls
+)
+
+:Main
+cd %TEMP%
+echo[
+echo[
+echo[
+echo                                                  ██████  ██████  ██    ██                                            
+echo                                                  ██      ██      ██    ██                                            
+echo                                                  ██████  ██████  ████████                                            
+echo                                                      ██      ██  ██    ██                                            
+echo                                                  ██████  ██████  ██    ██           Simple SSH Connection.cmd by Caver1K                                           
+echo[
+echo[
+echo[
+
+echo[
+echo[
+title Select Target/SSH Server
+set /p server=Target/SSH Server:
+echo[
+title Select Username
+set /p user=Username:
+echo[
+title Select Password
+set /p pass=Password:
+cls
+title SSH: Connected
+plink.exe -ssh %user%@%server% -pw "%pass%"
+if errorlevel 1 (
+	echo[
+	echo Connection failed with error code %errorlevel%, please try again
+	<nul set /p=Press any key to return...
+	pause >nul
+	cls
+	goto main 
+)
+if errorlevel 2 (
+	echo[
+	echo Connection was terminated unexpectedly with error code %errorlevel%, please try again
+	<nul set /p=Press any key to return...
+	pause >nul
+	cls
+	goto main
+)
+
+
+echo[
+echo Connection closed.
+timeout /t 3 >nul
+cls
+goto Main
